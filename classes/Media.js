bbf7476ef47100e7887d1d0b286dd61e301ababe
Scheltema <thomas.scheltema@hp.com>
2025-02-25 11:26:36 -0600

    Media tests passing

--- classes/Media.js
+++ classes/Media.js
@@ -1,4 +1,17 @@
 // create your Media class:
+class Media {
+  constructor(title, year, genre){
+    this.title = title;
+    this.year = year;
+    this.genre = genre;
+
+    Media.totalMediaCount++;
+  }
+  static totalMediaCount = 0
+  summary() {
+    return `Title: ${this.title}, Year: ${this.year}, Genre: ${this.genre}`
+  }
+}
 
 // uncomment below to export it:
 module.exports = Media;
